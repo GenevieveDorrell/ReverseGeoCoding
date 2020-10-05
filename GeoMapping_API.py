59fc6b11bc08d91e139b0904953a487f9da92b27
GenevieveDorrell <genna.dorrell@gmail.com>
2020-10-05 15:40:28 -0700

    hmm

--- GeoMapping_API.py
+++ GeoMapping_API.py
@@ -2,3 +2,17 @@ import requests
 import input
 
 #this accesses the API resource
+
+class address:
+    def __init__(number, street, state, zipCode):
+        self.number = number    
+        self.street = street
+        self.state = state
+        self.zipCode = zipCode
+
+url = "https://geoservices.tamu.edu/Services/ReverseGeocoding/WebService/v04_01/HTTP/default.aspx"
+apikey = '	4fec299ee59b4b2b8a38389b3d91e249'
+
+variables = {'lon': '-123.0817087', 'lat': '44.039158', 'format': 'csv', 'apikey': apikey, 'notStore': 'false', 'includeHeader': 'true', 'version': '4.10'}
+r = requests.get(url, params=variables)
+print(r.text)
