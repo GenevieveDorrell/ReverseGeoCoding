446406f192be2d361f46a86d9076ddf53b9738b9
Jacob Evarts <jevarts@uoregon.edu>
2020-10-06 19:42:24 -0700

    venv

--- GeoMapping_API.py
+++ GeoMapping_API.py
@@ -13,7 +13,7 @@ class address:
 url = "https://geoservices.tamu.edu/Services/ReverseGeocoding/WebService/v04_01/HTTP/default.aspx"
 apikey = '	4fec299ee59b4b2b8a38389b3d91e249'
 
-variables = {'lon': '-123.0817087', 'lat': '44.039158', 'format': 'csv', 'apikey': apikey, 'notStore': 'false', 'includeHeader': 'true', 'version': '4.10'}
+variables = {'lon': '-123.082144', 'lat': '44.040041', 'format': 'csv', 'apikey': apikey, 'notStore': 'false', 'includeHeader': 'true', 'version': '4.10'}
 r = requests.get(url, params=variables)
 parsed= r.text.split(',')
 print(parsed)
