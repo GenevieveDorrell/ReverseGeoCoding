d845dbb668f6d8b203831263040fb611fea25dc4
Jacob Evarts <jevarts@uoregon.edu>
2020-10-07 19:30:25 -0700

    UI

--- User_interface.py
+++ User_interface.py
@@ -3,18 +3,12 @@ import Directions
 def cli():
     inputf = ""
     while(inputf != "exit"):
-        try:
-            print("Enter the file name (including relative path) to your .gpx \ 
-                  data (ex. data/input.gpx)")
-            inputf = input("- ")
-            if inputf == "exit":
-                exit(1)
-        except FileNotFoundError:
-            print("Could not find file " + inputf + ", please try again or type 'exit'")
-            continue
-        
-        print(inputf)
-        get_latlon(inputf)
+        print("Enter the file name (including relative path) to your .gpx data (ex. data/input.gpx)")
+        inputf = input("- ")
+        if inputf == "exit":
+            exit(1)
+
+    return inputf
     
     
 if __name__ == "__main__":
