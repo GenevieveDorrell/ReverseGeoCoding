d2d38b784f1fb691be7eae4d5b0ec756151bbc71
GenevieveDorrell <genna.dorrell@gmail.com>
2020-10-07 18:46:40 -0700

    commit

--- GeoMapping_API.py
+++ GeoMapping_API.py
@@ -3,21 +3,18 @@ import requests
 #this accesses the API resource
 
 class address:
-    def __init__(self, number, street, city, state, zipCode, lat, lon):
-        self.number = number    
+    def __init__(self, street, lat, lon, index):  
         self.street = street
-        self.city = city
-        self.state = state
-        self.zipCode = zipCode
         self.lat = lat
         self.lon = lon
+        self.index = index
     def __repr__(self):
         return "number = % s\nstreet = % s\ncity = % s\nstate = % s\nzipCode = % s\nlat = % s\nlon = % s\n" % (self.number,self.street,self.city,self.state,self.zipCode,self.lat,self.lon)
 
 url = "https://geoservices.tamu.edu/Services/ReverseGeocoding/WebService/v04_01/HTTP/default.aspx"
 apikey = '4fec299ee59b4b2b8a38389b3d91e249'
 
-def getpoint(lat, lon):
+def getpoint(lat, lon, index):
     variables = {'lon': lon, 'lat': lat, 'format': 'csv', 'apikey': apikey, 'notStore': 'false', 'includeHeader': 'false', 'version': '4.10'}
     r = requests.get(url, params=variables)
     parsed = r.text.split(',')
