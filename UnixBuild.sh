cc4b66c142966b092099dd0b6db64dcd90fd4f5e
GenevieveDorrell <genna.dorrell@gmail.com>
2020-10-20 18:49:09 -0700

    update

--- UnixBuild.sh
+++ UnixBuild.sh
@@ -1,6 +1,7 @@
 python3 -m pip install --user --upgrade pip
 python3 -m pip install --user virtualenv
 python3 -m venv env
+python3 getApiKey.py
 source env/bin/activate
 pip install -r requirements.txt
 source run.cmd
